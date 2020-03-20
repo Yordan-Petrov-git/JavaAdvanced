2fb846ce4883a6744911d962a5bbbd3930eb4551
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-03-20 10:46:21 +0200

    2 exc

--- src/Main.java
+++ src/Main.java
@@ -15,8 +15,31 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         //=========Exc 1
-       // browserHistroy();
-        //=========Exc  
+        // browserHistroy();
+        //=========Exc
+        ArrayDeque<Integer> calc = new ArrayDeque<>();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] symbols = input.split("\\s+");
+        calc.push(Integer.parseInt(symbols[0]));
+        if (symbols.length == 0) {
+            return;
+        }
+
+        for (int i = 1; i < symbols.length; i++) {
+            String operations = symbols[i];
+            int num = Integer.parseInt(symbols[++i]);
+            int stackNum =  calc.pop();
+
+            if ("+".equals(operations)){
+                calc.push(num+stackNum);
+            }else if ("-".equals(operations)){
+                calc.push(stackNum-num);
+            }
+
+        }
+        System.out.print(calc.peek());
+
     }
 
     private static void browserHistroy() {
